857328f63a3d1b38b86b2d9d9671abe74a824100
Quinchy <deguzmancyriljames@gmail.com>
2023-05-20 17:47:17 +0800

    Add project files.

---
+++ BankingSystem/Program.cs
@@ -0,0 +1,12 @@
+namespace BankingSystem
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Forms.LoginForm());
+        }
+    }
+}

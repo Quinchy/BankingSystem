ac16a34d1e815eafb7308c3accb2b78ec9b2ddd1
Quinchy <deguzmancyriljames@gmail.com>
2023-06-01 00:20:47 +0800

    add database connection

--- BankingSystem/Database/MySQLDatabase.cs
+++ BankingSystem/Database/MySQLDatabase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,52 @@ namespace BankingSystem.Database
 {
     internal class MySQLDatabase
     {
+        private static string server;
+        private static string database;
+        private static string uid;
+        private static string password;
+        static MySQLDatabase()
+        {
+            Initialize();
+        }
+        private static void Initialize()
+        {
+            server = "localhost";
+            database = "banking_system";
+            uid = "root"; //default XAMPP MySQL username
+            password = ""; //default XAMPP MySQL password
+        }
+        public static MySqlConnection OpenConnection()
+        {
+            try
+            {
+                string connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+                database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+                MySqlConnection connection = new MySqlConnection(connectionString);
+                connection.Open();
+                return connection;
+            }
+            catch (MySqlException ex)
+            {
+                //Output the exception details for debugging
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+        public static bool CloseConnection(MySqlConnection connection)
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //Output the exception details for debugging
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
     }
 }

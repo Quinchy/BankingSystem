6c6861f1c591247386b11a7b1fbb471544f14867
Quinchy <deguzmancyriljames@gmail.com>
2023-06-01 00:22:28 +0800

    authenticateUser now tells if password is correct or not.

--- BankingSystem/Services/LoginServices.cs
+++ BankingSystem/Services/LoginServices.cs
@@ -1,4 +1,5 @@
 ﻿using BankingSystem.Models;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +10,30 @@ namespace BankingSystem.Services
 {
     internal class LoginServices
     {
-        public static Boolean authenticateUser(String email, String password)
+        public static int authenticateUser(String email, String password)
         {
-            Boolean userExist = true;
-            // This method will check if the user is in the database.
-            // Open Connection
-            // Write an SQL statement to check if the user is in the database then change userExist to true.
-            // Close Connection
-            return userExist;
+            // Open connection
+            using (var connection = new MySqlConnection("server=localhost;userid=root;password=;database=banking_system"))
+            {
+                connection.Open();
+                // Write an SQL statement to check if the user is in the database
+                string query = $"SELECT COUNT(*) FROM customer WHERE email = @Email";
+                using (var command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Email", email);
+                    // Execute the query and get the result
+                    int count = Convert.ToInt32(command.ExecuteScalar());
+
+                    if (count == 0) return -1; // Email does not exist
+
+                    query = $"SELECT COUNT(*) FROM customer WHERE email = @Email AND password = @Password";
+                    command.CommandText = query;
+                    command.Parameters.AddWithValue("@Password", password);
+                    count = Convert.ToInt32(command.ExecuteScalar());
+
+                    return count > 0 ? 1 : 0; // If count > 0, password is correct, otherwise incorrect
+                }
+            }
         }
     }
 }

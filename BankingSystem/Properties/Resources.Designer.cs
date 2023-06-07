823f2f322a42fb73288a6b2b9d8d795b338c0384
Quinchy <deguzmancyriljames@gmail.com>
2023-06-08 01:29:23 +0800

    add a copy symbol

--- BankingSystem/Properties/Resources.Designer.cs
+++ BankingSystem/Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace BankingSystem.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        public static System.Drawing.Bitmap duplicate {
+            get {
+                object obj = ResourceManager.GetObject("duplicate", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         public static System.Drawing.Bitmap edit {
             get {
                 object obj = ResourceManager.GetObject("edit", resourceCulture);

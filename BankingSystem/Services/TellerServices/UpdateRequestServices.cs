5fca5139403a9b399682cbd5c0da640547496586
Quinchy <deguzmancyriljames@gmail.com>
2023-06-24 19:01:06 +0800

    add the method for customer update

--- BankingSystem/Services/TellerServices/UpdateRequestServices.cs
+++ BankingSystem/Services/TellerServices/UpdateRequestServices.cs
@@ -63,21 +63,139 @@ namespace BankingSystem.Services.TellerServices
             }
             return updates;
         }
-        public static void ApproveEmailChange(string updateId) 
-        { 
-        
+        public static void ApproveEmailChange(string updateId)
+        {
+            using (MySqlConnection connection = MySQLDatabase.OpenConnection())
+            {
+                string updateSql = @"
+                    UPDATE customer_update 
+                    SET update_status = 'Approved' 
+                    WHERE update_id = @UpdateId";
+                MySqlCommand updateCommand = new MySqlCommand(updateSql, connection);
+                updateCommand.Parameters.AddWithValue("@UpdateId", updateId);
+                updateCommand.ExecuteNonQuery();
+
+                string selectSql = @"
+                    SELECT account_id, changed_information 
+                    FROM customer_update 
+                    WHERE update_id = @UpdateId";
+                MySqlCommand selectCommand = new MySqlCommand(selectSql, connection);
+                selectCommand.Parameters.AddWithValue("@UpdateId", updateId);
+                MySqlDataReader reader = selectCommand.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    string accountId = reader.GetString("account_id");
+                    string changedEmail = reader.GetString("changed_information");
+                    reader.Close();
+
+                    string updateCustomerSql = @"
+                        UPDATE customer_information 
+                        SET email = @ChangedEmail 
+                        WHERE customer_id = (
+                        SELECT customer_id 
+                        FROM account 
+                        WHERE account_id = @AccountId)";
+                    MySqlCommand updateCustomerCommand = new MySqlCommand(updateCustomerSql, connection);
+                    updateCustomerCommand.Parameters.AddWithValue("@ChangedEmail", changedEmail);
+                    updateCustomerCommand.Parameters.AddWithValue("@AccountId", accountId);
+                    updateCustomerCommand.ExecuteNonQuery();
+                }
+            }
         }
         public static void ApprovePhoneNumberChange(string updateId)
         {
+            using (MySqlConnection connection = MySQLDatabase.OpenConnection())
+            {
+                string updateSql = @"
+                    UPDATE customer_update 
+                    SET update_status = 'Approved' 
+                    WHERE update_id = @UpdateId";
+                MySqlCommand updateCommand = new MySqlCommand(updateSql, connection);
+                updateCommand.Parameters.AddWithValue("@UpdateId", updateId);
+                updateCommand.ExecuteNonQuery();
 
+                string selectSql = @"
+                    SELECT account_id, changed_information 
+                    FROM customer_update 
+                    WHERE update_id = @UpdateId";
+                MySqlCommand selectCommand = new MySqlCommand(selectSql, connection);
+                selectCommand.Parameters.AddWithValue("@UpdateId", updateId);
+                MySqlDataReader reader = selectCommand.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    string accountId = reader.GetString("account_id");
+                    string changedPhoneNumber = reader.GetString("changed_information");
+                    reader.Close();
+
+                    string updateCustomerSql = @"
+                        UPDATE customer_information 
+                        SET phone_number = @ChangedPhoneNumber 
+                        WHERE customer_id = (
+                        SELECT customer_id 
+                        FROM account 
+                        WHERE account_id = @AccountId)";
+                    MySqlCommand updateCustomerCommand = new MySqlCommand(updateCustomerSql, connection);
+                    updateCustomerCommand.Parameters.AddWithValue("@ChangedPhoneNumber", changedPhoneNumber);
+                    updateCustomerCommand.Parameters.AddWithValue("@AccountId", accountId);
+                    updateCustomerCommand.ExecuteNonQuery();
+                }
+            }
         }
+
         public static void ApprovePasswordChange(string updateId)
         {
+            using (MySqlConnection connection = MySQLDatabase.OpenConnection())
+            {
+                string updateSql = @"
+                    UPDATE customer_update 
+                    SET update_status = 'Approved' 
+                    WHERE update_id = @UpdateId";
+                MySqlCommand updateCommand = new MySqlCommand(updateSql, connection);
+                updateCommand.Parameters.AddWithValue("@UpdateId", updateId);
+                updateCommand.ExecuteNonQuery();
 
+                string selectSql = @"
+                    SELECT account_id, changed_information 
+                    FROM customer_update 
+                    WHERE update_id = @UpdateId";
+                MySqlCommand selectCommand = new MySqlCommand(selectSql, connection);
+                selectCommand.Parameters.AddWithValue("@UpdateId", updateId);
+                MySqlDataReader reader = selectCommand.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    string accountId = reader.GetString("account_id");
+                    string changedPassword = reader.GetString("changed_information");
+                    reader.Close();
+
+                    string updateCustomerSql = @"
+                        UPDATE customer_information 
+                        SET password = @ChangedPassword 
+                        WHERE customer_id = (
+                        SELECT customer_id 
+                        FROM account 
+                        WHERE account_id = @AccountId)";
+                    MySqlCommand updateCustomerCommand = new MySqlCommand(updateCustomerSql, connection);
+                    updateCustomerCommand.Parameters.AddWithValue("@ChangedPassword", changedPassword);
+                    updateCustomerCommand.Parameters.AddWithValue("@AccountId", accountId);
+                    updateCustomerCommand.ExecuteNonQuery();
+                }
+            }
         }
         public static void RejectUpdate(string updateId)
         {
-
+            using (MySqlConnection connection = MySQLDatabase.OpenConnection())
+            {
+                string sql = @"
+                    UPDATE customer_update 
+                    SET update_status = 'Rejected' 
+                    WHERE update_id = @UpdateId";
+                MySqlCommand command = new MySqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@UpdateId", updateId);
+                command.ExecuteNonQuery();
+            }
         }
     }
 } 
